f7995b1cf293519031c29c2058f446b8ec522209
Ghufran Mochammad <ghumadd@gmail.com>
2015-10-30 17:21:09 +0700

    Vector3D, Update2

--- 01-Vector/Vector3D.java
+++ 01-Vector/Vector3D.java
@@ -17,7 +17,6 @@ public class Vector3D
      */
     public Vector3D(double x, double y, double z)
     {
-        // initialise instance variables
         this.x=x;
         this.y=y;
         this.z=z;
