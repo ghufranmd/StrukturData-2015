0c838d4af61dbd4ebd66840cfa9a6892ed139ef4
Ghufran Mochammad <ghumadd@gmail.com>
2015-11-06 14:45:08 +0700

    Profil-Test-Awal (Coding-04) 6/Nov/15

---
+++ 04-Profil-Test-Awal/Profil.java
@@ -0,0 +1,19 @@
+public class Profil
+{   
+    private String nama = "Ghufran Mochammad";
+    private String nim = "1408107010074";
+    public Profil(){}
+    public Profil(String nama, String nim)
+    {
+        this.nama=nama;
+        this.nim=nim;
+    }
+    public String getNama()
+    {
+        return nama;
+    }
+    public String getNim()
+    {
+        return nim;
+    }
+}
